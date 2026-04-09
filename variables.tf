0af8966b02a60b7842aafd41109e204f3a689d93
shemaikuzwe <eshemaikuzwe@gmail.com>
2026-04-09 18:12:55 +0200

    fix:register domain

--- variables.tf
+++ variables.tf
@@ -3,14 +3,14 @@ variable "cluster_name" {
 }
 
 variable "organization_name" {
- type = string
- default = "shemaIkuzwe"
+  type    = string
+  default = "shemaIkuzwe"
 }
 variable "environment" {
   type = string
 }
 
 variable "registry" {
-  default = "<enter-your-registry>"
+  default = "629049518246.dkr.ecr.us-east-1.amazonaws.com"
   type    = string
 }
